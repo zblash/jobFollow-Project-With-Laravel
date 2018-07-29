1eebb11a8141b05bbfe070d93a97d535840009d0
Yusuf Can Celik <yusufcancelik@hotmail.com>
2018-07-29 19:42:35 +0300

    Add to Project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Fix Cleaning - @yield('pageTitle')</title>
+  <title>İş Takip - @yield('pageTitle')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -234,9 +234,9 @@
 
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      Created By <b>Dcweb</b>
+      Created By <b>zblash</b>
     </div>
-    <strong>Copyright &copy; Fix Cleaning.</strong>
+    <strong>Copyright &copy; Is Takip.</strong>
   </footer>
 
   <!-- Control Sidebar -->
